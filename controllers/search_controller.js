6e5a244dbb00d06f605e73e184f072bfc5e9b4ee
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2022-03-13 15:24:02 +0530

    completed all the functionality of the Everlast Notes......Next phase making ready for production

--- controllers/search_controller.js
+++ controllers/search_controller.js
@@ -3,32 +3,25 @@ const Problem =require('../models/problem');
 const Document =require('../models/document');
 const Code =require('../models/code');
 const Task =require('../models/task');
-const bellData = require('./bell_controller');
+
+// in regex ^ will restrict to string starting with payload
 module.exports.searchProblems = async function(req,res){
     let payload = req.body.payload.trim();
-    // console.log(payload);
-    // in regex ^ will restrict to string starting with payload
     let searchResult = await Problem.find({name:{$regex: new RegExp('.*'+payload+'.*','i')},author:req.user}).exec();
     res.send({payload:searchResult});
 };
 module.exports.searchDocuments = async function(req,res){
     let payload = req.body.payload.trim();
-    // console.log(payload);
-    // in regex ^ will restrict to string starting with payload
     let searchResult = await Document.find({name:{$regex: new RegExp('.*'+payload+'.*','i')},author:req.user}).exec();
     res.send({payload:searchResult});
 };
 module.exports.searchCodes = async function(req,res){
     let payload = req.body.payload.trim();
-    // console.log(payload);
-    // in regex ^ will restrict to string starting with payload
     let searchResult = await Code.find({name:{$regex: new RegExp('.*'+payload+'.*','i')},author:req.user}).exec();
     res.send({payload:searchResult});
 };
 module.exports.searchTasks = async function(req,res){
     let payload = req.body.payload.trim();
-    // console.log(payload);
-    // in regex ^ will restrict to string starting with payload
     let searchResult = await Task.find({name:{$regex: new RegExp('.*'+payload+'.*','i')},author:req.user}).exec();
     res.send({payload:searchResult});
 };
